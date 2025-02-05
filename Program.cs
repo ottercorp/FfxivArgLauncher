c93897afb5bf9007660ddf34901e20446bba07f2
Loskh <1020612624@qq.com>
2025-02-05 21:37:51 +0800

    feat: use command line as args

--- Program.cs
+++ Program.cs
@@ -9,20 +9,42 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var noFixAcl = true;
-        var waitForGameWindow = true;
-        var gamePath = "D:\\Games\\FFXIV\\sdo\\sdologin\\..\\..\\game\\ffxiv_dx11.exe";
-        var gameArgumentString = "-AppID=100001900 -AreaID=7 Dev.LobbyHost01=ffxivlobby07.ff14.sdo.com Dev.LobbyPort01=54994 Dev.GMServerHost=ffxivgm07.ff14.sdo.com Dev.SaveDataBankHost=ffxivsdb07.ff14.sdo.com resetConfig=0 DEV.MaxEntitledExpansionID=1 XL.SndaId=1111 DEV.TestSID=OTTER-114514";
-        var process = GameStart.LaunchGame(
-                Path.GetDirectoryName(gamePath),
-                gamePath,
-                gameArgumentString,
-                noFixAcl,
-                p =>
+        foreach (var item in args)
+        {
+            Console.WriteLine(item);
+        }
+
+        switch (args.Length)
+        {
+            case 3:
                 {
-                    var argFixer = new ArgFixer(p);
-                    argFixer.Fix();
-                },
-                waitForGameWindow);
+                    if (args[0] == "launch")
+                    {
+                        var noFixAcl = true;
+                        var waitForGameWindow = true;
+                        var gamePath = args[1];
+                        var gameArgumentString = args[2];
+                        var process = GameStart.LaunchGame(
+                            Path.GetDirectoryName(gamePath),
+                            gamePath,
+                            gameArgumentString,
+                            noFixAcl,
+                            p =>
+                            {
+                                var argFixer = new ArgFixer(p);
+                                argFixer.Fix();
+                            },
+                            waitForGameWindow);
+                    }
+
+                    break;
+                }
+
+            default:
+                {
+                    throw new Exception("Error args");
+                    break;
+                }
+        }
     }
 }
