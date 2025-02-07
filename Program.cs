c799e2c04243c1033b26d2905b990e16f355bf48
Loskh <1020612624@qq.com>
2025-02-07 23:00:53 +0800

    remove arg reader

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Dalamud.Injector;
@@ -10,40 +10,20 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        // launch gamePath argments
-        // read pid channelName
-
-        switch (args[0])
-        {
-            case "launch":
-                {
-                    var noFixAcl = false;
-                    var waitForGameWindow = true;
-                    var gamePath = args[1];
-                    var gameArgumentString = args[2];
-                    var process = GameStart.LaunchGame(
-                        Path.GetDirectoryName(gamePath),
-                        gamePath,
-                        gameArgumentString,
-                        noFixAcl,
-                        p =>
-                        {
-                            var argFixer = new ArgFixer(p);
-                            argFixer.Fix();
-                        },
-                        waitForGameWindow);
-
-                    break;
-                }
-
-            case "read":
-                {
-                    var process = Process.GetProcessById(int.Parse(args[1]));
-                    var argReader = new ArgReader(process);
-                    break;
-                }
-            default:
-                throw new Exception("Error args");
-        }
+        var noFixAcl = false;
+        var waitForGameWindow = true;
+        var gamePath = args[0];
+        var gameArgumentString = args[1];
+        var process = GameStart.LaunchGame(
+            Path.GetDirectoryName(gamePath),
+            gamePath,
+            gameArgumentString,
+            noFixAcl,
+            p =>
+            {
+                var argFixer = new ArgFixer(p);
+                argFixer.Fix();
+            },
+            waitForGameWindow);
     }
 }
